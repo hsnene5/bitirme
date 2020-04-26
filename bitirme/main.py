7c4cbca2fd4bb73abee83ad04894b0af6da00eb1
hsnene5 <46786681+hsnene5@users.noreply.github.com>
2020-04-26 17:00:22 +0300

    some

--- bitirme/main.py
+++ bitirme/main.py
@@ -1,6 +1,5 @@
 
 from flask import Flask, render_template, url_for
-from OpenSSL import SSL
 context = SSL.Context(SSL.PROTOCOL_TLSv1_2)
 context.use_privatekey_file('server.key')
 context.use_certificate_file('server.crt')
