a75fb464ad193aaad3f2e8b18fee10455e13adae
hsnene5 <46786681+hsnene5@users.noreply.github.com>
2020-05-08 23:03:15 +0300

    interface edits, popups, simulation mode

--- bitirme/main.py
+++ bitirme/main.py
@@ -1,6 +1,6 @@
 
 
-from dronekit import connect, VehicleMode
+from dronekit import connect, VehicleMode, LocationGlobalRelative
 from pymavlink import mavutil
 from Queue import Queue
 from flask import Flask, render_template, jsonify, Response, request,url_for
@@ -17,41 +17,159 @@ from flask import render_template
 from flask import Flask, Response
 from datetime import datetime
 
+def sse_encode(obj, id=None):
+    return "data: %s\n\n" % json.dumps(obj)
+
+def state_msg():
+    if vehicle.location.global_relative_frame.lat == None:
+        raise Exception('no position info')
+    if vehicle.armed == None:
+        raise Exception('no armed info')
+    return {
+        "armed": vehicle.armed,
+        "alt": vehicle.location.global_relative_frame.alt,
+        "mode": vehicle.mode.name,
+        "heading": vehicle.heading or 0,
+        "lat": vehicle.location.global_relative_frame.lat,
+        "lon": vehicle.location.global_relative_frame.lon
+    }
+
+listeners_location = []
+listeners_location
+
+from threading import Thread
+import time
+def tcount():
+    while True:
+        time.sleep(0.25)
+        try:
+            msg = state_msg()
+            for x in listeners_location:
+                x.put(msg)
+        except Exception as e:
+            pass
+t = Thread(target=tcount)
+t.daemon = True
+t.start()
+
 vehicle = None
 app = Flask(__name__)
 
+@app.route("/api/sse/state")
+def api_sse_location():
+    def gen():
+        q = Queue()
+        listeners_location.append(q)
+        try:
+            while True:
+                result = q.get()
+                ev = sse_encode(result)
+                yield ev.encode()
+        except GeneratorExit: # Or maybe use flask signals
+            listeners_location.remove(q)
+
+    return Response(gen(), mimetype="text/event-stream")
+
+
+
+
 @app.route("/")
 def home():
-    return render_template('landingPage.html', branding = False)
+    return render_template('autoModeModal.html', branding = False)
 
 @app.route("/api/guided", methods = ['POST','PUT'])
 def api_guided():
+    """
+    Arms vehicle and fly to aTargetAltitude.
+    """
+
+    print("Basic pre-arm checks")
+    # Don't try to arm until autopilot is ready
     while not vehicle.is_armable:
-        print "Waiting for vehicle to become armable"
+        print(" Waiting for vehicle to initialise...")
         time.sleep(1)
 
-    #Switch vehicle to GUIDED mode and wait for change
+    print("Arming motors")
+    # Copter should arm in GUIDED mode
     vehicle.mode = VehicleMode("GUIDED")
-    while vehicle.mode!="GUIDED":
-        print "Waiting for vehicle to enter GUIDED mode"
+    vehicle.armed = True
+    aTargetAltitude = 10
+    # Confirm vehicle armed before attempting to take off
+    while not vehicle.armed:
+        print(" Waiting for arming...")
         time.sleep(1)
 
-    #Arm vehicle once GUIDED mode is confirmed
-    vehicle.armed=True
-    while vehicle.armed==False:
-        print "Waiting for vehicle to become armed."
-        time.sleep(1)
+    print("Taking off!")
+    vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
 
-    vehicle.simple_takeoff(1)
-
+    # Wait until the vehicle reaches a safe height before processing the goto
+    #  (otherwise the command after Vehicle.simple_takeoff will execute
+    #   immediately).
     while True:
-        print("Current Altitude: %d"%vehicle.location.global_relative_frame.alt)
-        if vehicle.location.global_relative_frame.alt>=1*.95:
+        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+        # Break and return from function just below target altitude.
+        if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
+            print("Reached target altitude")
+            break
+        time.sleep(1)
+    return jsonify(ok=True)
+
+@app.route("/api/auto", methods = ['POST','PUT'])
+def api_autoMode():
+    print("Basic pre-arm checks")
+    # Don't try to arm until autopilot is ready
+    while not vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
+
+    print("Arming motors")
+    # Copter should arm in GUIDED mode
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True
+
+    # Confirm vehicle armed before attempting to take off
+    while not vehicle.armed:
+        print(" Waiting for arming...")
+        time.sleep(1)
+
+    print("Taking off!")
+    vehicle.simple_takeoff(10)  # Take off to target altitude
+
+    # Wait until the vehicle reaches a safe height before processing the goto
+    #  (otherwise the command after Vehicle.simple_takeoff will execute
+    #   immediately).
+    while True:
+        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+        # Break and return from function just below target altitude.
+        if vehicle.location.global_relative_frame.alt >= 10 * 0.95:
+            print("Reached target altitude")
             break
         time.sleep(1)
 
-    print("Target altitude reached")
-    return None
+    print("Set default/target airspeed to 3")
+    vehicle.airspeed = 3
+
+    print("Going towards first point for 30 seconds ...")
+    point1 = LocationGlobalRelative(39.9853521, 32.6448407, 20)
+    vehicle.simple_goto(point1)
+
+    # sleep so we can see the change in map
+    time.sleep(30)
+    
+    print("Going towards second point for 30 seconds (groundspeed set to 10 m/s) ...")
+    point2 = LocationGlobalRelative(-35.363244, 149.168801, 20)
+    vehicle.simple_goto(point2, groundspeed=10)
+    
+    # sleep so we can see the change in map
+    time.sleep(30)
+    
+    print("Returning to Launch")
+    vehicle.mode = VehicleMode("RTL")
+    
+    # Close vehicle object before exiting script
+    print("Close vehicle object")
+    vehicle.close()
+    return jsonify(ok=True)
 
 @app.route("/api/arm", methods=['POST', 'PUT'])
 def api_arm():
@@ -64,25 +182,37 @@ def api_arm():
             print(e)
             return jsonify(ok=False)
 
+@app.route("/api/connect", methods=['POST','PUT'])
 def connect_to_drone():
-    global vehicle
+    if request.method == 'POST' or request.method == 'PUT':
 
-    print 'connecting to drone...'
-    while not vehicle:
-        try:
-            vehicle = connect('com7', wait_ready=False,baud=57600)
-        except Exception as e:
-            print 'waiting for connection... (%s)' % str(e)
-            time.sleep(2)
+        global vehicle
+
+        print 'connecting to drone...'
+        while not vehicle:
+            try:
+                vehicle = connect('com7', wait_ready=False,baud=57600)
+            except Exception as e:
+                print 'waiting for connection... (%s)' % str(e)
+                time.sleep(2)
 
     # if --sim is enabled...
 
-
     print 'connected!'
 
-t2 = Thread(target=connect_to_drone)
-t2.daemon = True
-t2.start()
+@app.route("/api/simulation", methods=['POST','PUT'])
+def enableSimulation():
+    import dronekit_sitl
+    sitl = dronekit_sitl.start_default()
+    global vehicle
+    connection_string = sitl.connection_string()
+    vehicle = connect(connection_string, wait_ready=True)
+    print 'simulation mode enabled'
+    return jsonify(ok=True)
+
+#t2 = Thread(target=connect_to_drone)
+#t2.daemon = True
+#t2.start()
 
 def main():
     app.config['TEMPLATE_AUTO_RELOAD'] = True
--- bitirme/static/scripts/scripts.js
+++ bitirme/static/scripts/scripts.js
@@ -27,6 +27,65 @@
 
     
 })(jQuery);*/
+
+//var map = new ol.Map({
+//    // controls: [],
+//    // interactions : ol.interaction.defaults({
+//    //   doubleClickZoom: false,
+//    //   dragZoom: false,
+//    //   keyboardZoom: false,
+//    //   mouseWheelZoom: false,
+//    //   pinchZoom: false,
+//    // }),
+//    target: 'googleMap',
+//    renderer: 'canvas', // Force the renderer to be used
+//    layers: [
+//        new ol.layer.Tile({
+//            source: new ol.source.BingMaps({
+//                key: 'AnGHr16zmRWug0WA8mJKrMg5g6W4GejzGPBdP-wQ4Gqqw-yHNqsHmYPYh1VUOR1q',
+//                imagerySet: 'AerialWithLabels',
+//                // imagerySet: 'Road',
+//            })
+//        })
+//    ],
+//    view: new ol.View({
+//        center: ol.proj.transform([39.9853521, 32.6448407], 'EPSG:4326', 'EPSG:3857'),
+//        zoom: 18
+//    })
+//});
+
+//var globmsg = null;
+
+//var source = new EventSource('/api/sse/state');
+//source.onmessage = function (event) {
+//    var msg = JSON.parse(event.data);
+//    if (!globmsg) {
+//        console.log('FIRST', msg);
+//        $('body').removeClass('disabled')
+//        map.getView().setCenter(ol.proj.transform([msg.lon, msg.lat], 'EPSG:4326', 'EPSG:3857'));
+//    }
+//    globmsg = msg;
+
+//    //$('#header-state').html('<b>Armed:</b> ' + msg.armed + '<br><b>Mode:</b> ' + msg.mode + '<br><b>Altitude:</b> ' + msg.alt.toFixed(2))
+//    //$('#header-arm').prop('disabled', msg.armed);
+
+//    //overlay.setPosition(ol.proj.transform([msg.lon, msg.lat], 'EPSG:4326', 'EPSG:3857'));
+//    //$(overlay.getElement()).find('.heading').css('-webkit-transform', 'rotate(' + ((msg.heading) + 45) + 'deg)')
+//};
+
+//$('#header-center').on('click', function () {
+//    map.getView().setCenter(ol.proj.transform([globmsg.lon, globmsg.lat], 'EPSG:4326', 'EPSG:3857'));
+//})
+
+
+$('.popover-dismiss').popover({
+    trigger: 'focus'
+})
+
+$(function () {
+    $('[data-toggle="popover"]').popover()
+})
+
 $('#arm').on('click', function () {
     $.ajax({
         method: 'PUT',
@@ -38,6 +97,18 @@ $('#arm').on('click', function () {
             console.log('sent arming message')
         });
     document.getElementById("arm").innerHTML="armed";
+})
+
+$('#simulation').on('click', function () {
+    $.ajax({
+        method: 'PUT',
+        url: '/api/simulation',
+        contentType: 'application/json',
+        data: JSON.stringify({ arm: true }),
+    })
+        .done(function (msg) {
+            console.log('sent arming message')
+        });
 })
 
 $('#guided').on('click', function () {
@@ -53,18 +124,19 @@ $('#guided').on('click', function () {
 })
 
 $('#auto').on('click', function () {
-    var modal = document.getElementById("autoModePopup");
-    modal.style.display = "block";
     $.ajax({
         method: 'PUT',
-        url: '/api/guided',
+        url: '/api/auto',
         contentType: 'application/json',
         data: JSON.stringify({ mode: 'GUIDED' }),
     })
         .done(function (msg) {
             console.log('sent guided mode')
         });
+
 })
+
+
 
 var modal = document.getElementById("autoModePopup");
 var span = document.getElementById("closeModal");
@@ -72,7 +144,7 @@ var span = document.getElementById("closeModal");
 
 var closeModal = document.getElementById("closeModal");
 // When the user clicks on <span> (x), close the modal
-closeModal.onclick = function () {
+/*closeModal.onclick = function () {
     modal.style.display = "none";
 }
 
