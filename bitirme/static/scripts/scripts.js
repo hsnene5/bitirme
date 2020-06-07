3f69ecd71517fc64ee92139716e67a6e22354434
hsnene5 <46786681+hsnene5@users.noreply.github.com>
2020-06-07 09:17:31 +0300

    sidebar toggle button fix.

--- bitirme/static/scripts/scripts.js
+++ bitirme/static/scripts/scripts.js
@@ -1,3 +1,20 @@
+(function ($) {
+    "use strict";
+   
+    // Add active state to sidbar nav links
+    var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+    $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function () {
+        if (this.href === path) {
+            $(this).addClass("active");
+        }
+    });
+
+    // Toggle the side navigation
+    $("#sidebarToggle").on("click", function (e) {
+        e.preventDefault();
+        $("body").toggleClass("sb-sidenav-toggled");
+    });
+})(jQuery);
 
 var map2; //Will contain map object.
 var marker2 = false; ////Has the user plotted their location marker? 
@@ -444,6 +461,7 @@ $('#cancelStart').on('click', function () {
 })
 
 $('#simulationSelectMapButton').on('click', function () {
+    console.log("simul");
     var elem = document.getElementById('simulationSelectMap');
     if (elem.style.display == "block") {
         elem.style.display = "none";
