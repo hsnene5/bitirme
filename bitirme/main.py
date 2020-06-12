4c9bd9195253313d1ab2cf31ab3dd17fb1368c31
hsnene5 <46786681+hsnene5@users.noreply.github.com>
2020-06-12 11:03:12 +0300

    Database managed. Notifications handled. Connection fixed.

--- bitirme/main.py
+++ bitirme/main.py
@@ -15,11 +15,28 @@ import glob
 import serial.tools.list_ports
 from threading import Thread
 from subprocess import Popen
-from flask import render_template
-from flask import Flask, Response
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from array import *
 
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///theUactDB.db'
+
+db = SQLAlchemy(app)
+
+class GuidedFlight(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    targetLat = db.Column(db.Float, nullable=False)
+    targetLon = db.Column(db.Float, nullable=False)
+    run_velocity = db.Column(db.Float, nullable=False)
+    run_altitude = db.Column(db.Float, nullable=False)
+    afterReach = db.Column(db.Text, nullable=False)
+    run_date = db.Column(db.DateTime, nullable=False, default=datetime.now)
+
+    def as_dict(self):
+        #return '[id:'+ self.id +',targetLat:'+self.targetLat+',targetLon:'+self.targetLon+',run_velocity:'+self.run_velocity+',run_altitude:'+self.run_altitude+',run_date:',self.run_date+']'
+        return {c.name: unicode(getattr(self, c.name)) for c in self.__table__.columns}
 
 vehicleState = None
 vehicle = None
@@ -62,9 +79,6 @@ t = Thread(target=tcount)
 t.daemon = True
 t.start()
 
-
-app = Flask(__name__)
-
 @app.route("/api/sse/state")
 def api_sse_location():
     def gen():
@@ -80,9 +94,6 @@ def api_sse_location():
 
     return Response(gen(), mimetype="text/event-stream")
 
-
-
-
 @app.route("/")
 def home():
     return render_template('autoModeModal.html', branding = False)
@@ -97,7 +108,7 @@ def api_guided():
     velocity = float(parameter["velocity"])
     point1Lat = float(parameter["point1Lat"])
     point1Lon = float(parameter["point1Lon"])
-    afterArrival = parameter["afterArrival"];
+    afterArrival = parameter["afterArrival"]
     print velocity
     print targetAltitude
     
@@ -149,6 +160,12 @@ def api_guided():
         api_land()
     if afterArrival == "LOITER":
         api_loiter()
+
+    flight = GuidedFlight(targetLat=point1Lat,targetLon=point1Lon,run_velocity=velocity,run_altitude=targetAltitude,afterReach=afterArrival)
+
+    db.session.add(flight)
+    db.session.commit()
+
     return jsonify(ok=True)
 
 @app.route("/api/auto", methods = ['POST','PUT'])
@@ -276,17 +293,9 @@ def api_loiter():
     
     print("Loiter mode is on")
     vehicle.mode = VehicleMode("STABILIZE")
-    vehicle.location.global_relative_frame.alt = 3
-    while True:
-        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
-        # Break and return from function just below target altitude.
-        ##if vehicle.mode != VehicleMode("LOITER"):
-          ##  print("Loiter mode is ended")
-            ##break
-        time.sleep(1)
+    
     # Close vehicle object before exiting script
-    print("Close vehicle object")
-    vehicle.close()
+
     return jsonify(ok=True)
 
 @app.route("/api/cancel", methods=['POST', 'PUT'])
@@ -305,11 +314,9 @@ def api_cancel():
 
 @app.route("/api/rtl", methods=['POST', 'PUT'])
 def api_rtl():
-
     vehicle.parameters['RTL_ALT'] = 0
     vehicle.mode = VehicleMode("RTL")
     print("Coming home!")
-
 
     return jsonify(ok=True)
 
@@ -374,151 +381,22 @@ def enableSimulation():
     
     return jsonify(ok=True)
 
+@app.route("/api/getDB", methods=['POST','PUT'])
+def getDB():
+    users = GuidedFlight.query.all()
+    for user in users:
+        return jsonify([u.as_dict() for u in GuidedFlight.query.all()])
+
 #t2 = Thread(target=connect_to_drone)
 #t2.daemon = True
 #t2.start()
-
-
-
-### from dronekit-python examples
-
-#    NO 1 
-#   The function is useful when you want to move the vehicle around specifying locations relative to 
-#    the current vehicle position.
-
-
-def get_location_metres(original_location, dNorth, dEast):
-  
-    earth_radius = 6378137.0 #Radius of "spherical" earth
-    #Coordinate offsets in radians
-    dLat = dNorth/earth_radius
-    dLon = dEast/(earth_radius*math.cos(math.pi*original_location.lat/180))
-
-    #New position in decimal degrees
-    newlat = original_location.lat + (dLat * 180/math.pi)
-    newlon = original_location.lon + (dLon * 180/math.pi)
-    if type(original_location) is LocationGlobal:
-        targetlocation=LocationGlobal(newlat, newlon,original_location.alt)
-    elif type(original_location) is LocationGlobalRelative:
-        targetlocation=LocationGlobalRelative(newlat, newlon,original_location.alt)
-    else:
-        raise Exception("Invalid Location object passed")
-        
-    return targetlocation;
-
-# NO 2
-
-def arm_and_takeoff(aTargetAltitude):
-    """
-    Arms vehicle and fly to aTargetAltitude.
-    """
-
-    print("Basic pre-arm checks")
-    # Don't let the user try to arm until autopilot is ready
-    while not vehicle.is_armable:
-        print(" Waiting for vehicle to initialise...")
-        time.sleep(1)
-
-        
-    print("Arming motors")
-    # Copter should arm in GUIDED mode
-    vehicle.mode = VehicleMode("GUIDED")
-    vehicle.armed = True
-
-    while not vehicle.armed:      
-        print(" Waiting for arming...")
-        time.sleep(1)
-
-    print("Taking off!")
-    vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
-
-    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command 
-    #  after Vehicle.simple_takeoff will execute immediately).
-    while True:
-        print(" Altitude: ", vehicle.location.global_relative_frame.alt)      
-        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95: #Trigger just below target alt.
-            print("Reached target altitude")
-            break
-        time.sleep(1)
-
-
-## NO 3
-
-def goto_position_target_global_int(aLocation):
-    """
-    Send SET_POSITION_TARGET_GLOBAL_INT command to request the vehicle fly to a specified LocationGlobal.
-
-    For more information see: https://pixhawk.ethz.ch/mavlink/#SET_POSITION_TARGET_GLOBAL_INT
-
-    See the above link for information on the type_mask (0=enable, 1=ignore). 
-    At time of writing, acceleration and yaw bits are ignored.
-    """
-    msg = vehicle.message_factory.set_position_target_global_int_encode(
-        0,       # time_boot_ms (not used)
-        0, 0,    # target system, target component
-        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT_INT, # frame
-        0b0000111111111000, # type_mask (only speeds enabled)
-        aLocation.lat*1e7, # lat_int - X Position in WGS84 frame in 1e7 * meters
-        aLocation.lon*1e7, # lon_int - Y Position in WGS84 frame in 1e7 * meters
-        aLocation.alt, # alt - Altitude in meters in AMSL altitude, not WGS84 if absolute or relative, above terrain if GLOBAL_TERRAIN_ALT_INT
-        0, # X velocity in NED frame in m/s
-        0, # Y velocity in NED frame in m/s
-        0, # Z velocity in NED frame in m/s
-        0, 0, 0, # afx, afy, afz acceleration (not supported yet, ignored in GCS_Mavlink)
-        0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink) 
-    # send command to vehicle
-    vehicle.send_mavlink(msg)
-
-# NO 4
-
-def get_distance_metres(aLocation1, aLocation2):
-    """
-    Returns the ground distance in metres between two LocationGlobal objects.
-
-    This method is an approximation, and will not be accurate over large distances and close to the 
-    earth's poles. It comes from the ArduPilot test code: 
-    """
-
-    dlat = aLocation2.lat - aLocation1.lat
-    dlong = aLocation2.lon - aLocation1.lon
-    return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
-
-def goto_position_target_global_int(aLocation):
-    """
-    Send SET_POSITION_TARGET_GLOBAL_INT command to request the vehicle fly to a specified LocationGlobal.
-
-    For more information see: https://pixhawk.ethz.ch/mavlink/#SET_POSITION_TARGET_GLOBAL_INT
-
-    See the above link for information on the type_mask (0=enable, 1=ignore). 
-    At time of writing, acceleration and yaw bits are ignored.
-    """
-    msg = vehicle.message_factory.set_position_target_global_int_encode(
-        0,       # time_boot_ms (not used)
-        0, 0,    # target system, target component
-        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT_INT, # frame
-        0b0000111111111000, # type_mask (only speeds enabled)
-        aLocation.lat*1e7, # lat_int - X Position in WGS84 frame in 1e7 * meters
-        aLocation.lon*1e7, # lon_int - Y Position in WGS84 frame in 1e7 * meters
-        aLocation.alt, # alt - Altitude in meters in AMSL altitude, not WGS84 if absolute or relative, above terrain if GLOBAL_TERRAIN_ALT_INT
-        0, # X velocity in NED frame in m/s
-        0, # Y velocity in NED frame in m/s
-        0, # Z velocity in NED frame in m/s
-        0, 0, 0, # afx, afy, afz acceleration (not supported yet, ignored in GCS_Mavlink)
-        0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink) 
-    # send command to vehicle
-    vehicle.send_mavlink(msg)
-
-t = Thread(target=tcount)
-t.daemon = True
-t.start()
-
 
 ###################################
 ## TARGET POINT REACHED FUNCTION ##
 ###################################
 def targetReached(point1, point2):
-    if "{:.6f}".format(point1.lat) == "{:.6f}".format(point2.lat):
-        if "{:.6f}".format(point1.lon) == "{:.6f}".format(point2.lon):
+    if "{:.5f}".format(point1.lat) == "{:.5f}".format(point2.lat):
+        if "{:.5f}".format(point1.lon) == "{:.5f}".format(point2.lon):
             return True
     else:
         return False
@@ -549,6 +427,7 @@ def main():
     app.config['TEMPLATE_AUTO_RELOAD'] = True
     app.config["CACHE_TYPE"] = "null"
     app.jinja_env.auto_reload = True
+    db.create_all()
     app.run(threaded=True,host='127.0.0.1', port=5000)
     
 
